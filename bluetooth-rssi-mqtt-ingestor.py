aa2afef992cdb4883c1c85837301fd83f69f19bc
husain3 <arshad.husain1@gmail.com>
2022-09-29 18:50:12 -0600

    Add CSV writing for bluetooth RSSI data

--- bluetooth-rssi-mqtt-ingestor.py
+++ bluetooth-rssi-mqtt-ingestor.py
@@ -1,5 +1,9 @@
 import json
 import paho.mqtt.client as mqtt
+import datetime
+import csv
+
+column_names = ['rssi', 'distance', 'datetime']
 
 # Open configuration json file
 with open('config.json', 'r') as configfile:
@@ -16,6 +20,12 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
+    rssi_distance_data = {}
+
+    with open('rssi_distance_data.csv', 'a') as csv_file:
+        dict_object = csv.DictWriter(csv_file, fieldnames=column_names)
+        dict_object.writerow(rssi_distance_data)
+
 
 client = mqtt.Client()
 client.on_connect = on_connect

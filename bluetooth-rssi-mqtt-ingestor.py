778479425f50df18b43af9d76994fcc1865d6a51
husain3 <arshad.husain1@gmail.com>
2022-09-29 18:33:54 -0600

    Read config json file for MQTT attributes

--- bluetooth-rssi-mqtt-ingestor.py
+++ bluetooth-rssi-mqtt-ingestor.py
@@ -1,11 +1,17 @@
+import json
 import paho.mqtt.client as mqtt
+
+# Open configuration json file
+with open('config.json', 'r') as configfile:
+    # Reading from json file
+    config_dict = json.load(configfile)
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("home/lights/kitchen")
+    client.subscribe(config_dict['mqtt_topic'])
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -15,7 +21,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("", 1883, 60)
+client.connect(config_dict['mqtt_broker_ip'], 1883, 60)
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
